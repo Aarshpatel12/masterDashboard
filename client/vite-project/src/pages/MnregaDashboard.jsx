66bda8114a2717c862b9b5369fceac1fac7f16a6
aarsh <144159115+Aarshpatel12@users.noreply.github.com>
2026-02-19 14:54:42 +0530

    Added graphs

--- client/vite-project/src/pages/MnregaDashboard.jsx
+++ client/vite-project/src/pages/MnregaDashboard.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 function MnregaDashboard() {
   return (
-    <div> This id MnregaDashboard
-      <h1>Hello From Manisha</h1>
-    </div>
-    <div> This id MnregaDashboard</div>
+    <div>MnregaDashboard</div>
+    
   )
 }
 
